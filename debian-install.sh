adb1c931a7b4dc247ffb19996483d1bbb8f5d0d7
giovanifss <11573379+giovanifss@users.noreply.github.com>
2018-04-25 18:18:03 -0300

    Add debian install script

---
+++ debian-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update && \
+  sudo apt-get install python3 python3-pip libgit2-dev cmake make wget openssl libssl-dev libffi-dev -y
+
+wget https://github.com/libgit2/libgit2/archive/v0.27.0.tar.gz && \
+  tar xzf v0.27.0.tar.gz && \
+  cd libgit2-0.27.0/ && \
+  cmake . && \
+  make && \
+  sudo make install
+
+ldconfig
+pip3 install pygit2
